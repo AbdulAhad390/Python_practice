2ce6729913f09918b34b4f34aadad5ecc238c44b
Ahad <a.ahad304567@gmail.com>
2025-09-16 00:56:18 +0500

    add some practices

---
+++ Chap 2/practice_1.py
@@ -0,0 +1,14 @@
+a  = 37
+b = 7
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+g = a % b
+
+print("The sum of a and b is: ", c)
+print("The difference of a and b is: ", d)
+print("The product of a and b is: ", e)
+print("The quotient of a and b is: ", f)
+print("The remainder of a and b is: ", g)
+
---
+++ Chap 2/practice_3.py
@@ -0,0 +1,13 @@
+a = int(input("Enter number 1: "))
+
+b = int(input("Enter number 2: "))
+
+print("a equals b: ", a == b)
+print("a not equals b: ", a != b)
+print("a greater than b: ", a > b)
+print("a less than b: ", a < b)
+print("a greater than or equals b: ", a >= b)
+print("a less than or equals b: ", a <= b)
+print("Average of a and b is: ", (a+b)/2)
+print("square of a is ", a ** 2)
+print("square of b is ", b * b)
