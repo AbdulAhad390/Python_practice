88ccb36c22fa4103d836091c657b198ea62659ae
Ahad <a.ahad304567@gmail.com>
2025-10-06 21:10:28 +0500

    add list

---
+++ Chap 4/list.py
@@ -0,0 +1,5 @@
+friends = ["Joseph", "Glenn", "philip" , "mark" , "Alex" , "Tom"]
+print(friends[0])
+friends[0] = "Sarah"
+print(friends[0])
+print(friends[1:4])
---
+++ Chap 4/list_methods.py
@@ -0,0 +1,15 @@
+friends = ["Joseph", "Glenn", "philip" , "mark" , "Alex" , "Tom"]
+print(friends)
+friends.append("mike")
+print(friends)
+
+l1 = [432,232,332,23,43,32,567]
+l1.sort()
+print(l1)
+l1.reverse()
+print(l1)
+l1.insert(2, 2222) # insert 2222 at index 2
+print(l1)
+value = l1.pop(1) # remove element at index 1
+print(value)
+print(l1)
