0002b16699c737d0b31ac00af9113a4b01b8160e
Ahad <a.ahad304567@gmail.com>
2025-12-18 23:57:11 +0500

    changes in list.py

--- Chap 4/list.py
+++ Chap 4/list.py
@@ -3,3 +3,4 @@ print(friends[0])
 friends[0] = "Sarah"
 print(friends[0])
 print(friends[1:4])
+print(friends[-1])
--- Chap 4/tuple_methods.py
+++ Chap 4/tuple_methods.py
@@ -7,3 +7,11 @@ print(b)
 
 i = a.index(2134)
 print(i)
+
+c = list(a)
+print(c)
+print(type(c))
+
+d = tuple(c)
+print(d)
+print(type(d))
